111be8b93975256089493da4fc063080fd345a6b
acailly <ylliac14@gmail.com>
2018-03-07 09:00:32 +0100

    :tada: First Version

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # bot-schedule
+
 Schedule commands
+
+## Config (config.yaml)
+
+```yaml
+schedule:
+  jobs:
+    - pattern: "once every minute"
+      command: "proxy"
+    - pattern: "once every hour"
+      command: "say hello"
+```
---
+++ index.js
@@ -0,0 +1,47 @@
+const chalk = require("chalk");
+const jsonfile = require("jsonfile");
+const later = require("later");
+const path = require("path");
+
+const startJobs = vorpal => {
+  const { jobs } = vorpal.config.schedule;
+
+  jobs.forEach(jobProps => {
+    startJob(jobProps, vorpal);
+  });
+};
+
+const startJob = ({ command, pattern }, vorpal) => {
+  const isPatternText = later.parse.text(pattern).error === -1;
+
+  let sched;
+  let humanSched;
+  if (isPatternText) {
+    sched = later.parse.text(pattern);
+    humanSched = pattern;
+  } else {
+    sched = later.parse.cron(pattern);
+    humanSched = `on '${pattern}'`;
+  }
+
+  const jobFunction = () => {
+    vorpal
+      .exec(command)
+      .then(res => {
+        if (res) vorpal.log(res);
+      })
+      .catch(error => {
+        if (error) console.log("error", error);
+      });
+  };
+
+  vorpal.log(
+    chalk.yellow(`[SCHEDULED] Executing command '${command}' ${humanSched}`)
+  );
+  later.setInterval(jobFunction, sched);
+};
+
+module.exports = function(vorpal) {
+  later.date.localTime();
+  startJobs(vorpal);
+};
